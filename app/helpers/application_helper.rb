45c7de73c349b037956a99f4e6e9fb92b62b1747
HeshamAbdalla <shamoh702@gmail.com>
2015-10-26 21:28:40 -0700

    custome error messages and style

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,15 @@
 module ApplicationHelper
+  def devise_error_messages!
+    return '' if resource.errors.empty?
+
+    messages = resource.errors.full_messages.map { |msg|
+    content_tag(:li, msg) }.join
+    html = <<-HTML
+    <div class="alert alert-error alert-danger">
+      <a href="#" class="close" data-dismiss='alert' &#215;></a>
+      <%= content_tag :div, msg if msg.is_a?(String) %>
+    </div>
+    HTML
+    html.html_safe
+  end
 end
